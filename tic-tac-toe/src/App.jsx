bcf7f3790974d2a4b3f82150fe7f723a3131f5ee
Jose Armas <armasjose606@gmail.com>
2023-07-10 12:52:33 -0400

    Add: win condition
    Stop game at win condition

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -23,19 +23,54 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const win_condition = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null))
   
   const [turn, setTurn] = useState(TURNS.X)
 
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (const condition of win_condition) {
+      const [a, b, c] = condition
+      if (
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[b] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
   const updateBoard = (index) => {
+
+    if (board[index] || winner) return 
+
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-    
+
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
    }
 
     return (
